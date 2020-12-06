8c8781179556bc71f60010384413b586c8b2dced
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-12-06 22:37:19 +0500

    drawers
    initials in drawers

---
+++ drawerui/lib/info.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'drawer.dart';
+
+class info extends StatefulWidget {
+  @override
+  _infoState createState() => _infoState();
+}
+
+class _infoState extends State<info> {
+  @override
+  Widget build(BuildContext context) {
+    return UserAccountsDrawerHeader(
+      accountName: Text("Muhammad Ahmer Khan"),
+      accountEmail: Text("mahmerk99@gmail.com"),
+      currentAccountPicture: CircleAvatar(
+        backgroundImage: AssetImage("assets/khan.jpeg"),
+        backgroundColor: Colors.grey,
+
+      ),
+    );
+  }
+}
+
+
